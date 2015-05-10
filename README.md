93c7ead8c326d7f231987d758547a142041cb2f8
Mario Škrlec <whitepostmail@gmail.com>
2015-05-10 11:18:56 +0200

    Initial commit on symfony2 installation

---
+++ README.md
@@ -0,0 +1,4 @@
+locastic
+========
+
+A Symfony project created on May 10, 2015, 10:40 am.
---
+++ src/Locastic/PublicBundle/LocasticPublicBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Locastic\PublicBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LocasticPublicBundle extends Bundle
+{
+}
